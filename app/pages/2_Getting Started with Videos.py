13c5d6024c8c1a72c78bf858dec1d066779513b8
Zehan <zehan.kareem@gmail.com>
2024-02-05 23:36:16 +0600

    placing start button in the page

--- app/pages/2_Getting Started with Videos.py
+++ app/pages/2_Getting Started with Videos.py
@@ -12,32 +12,35 @@ st.markdown("""
             unsafe_allow_html=True)
 
 st.subheader("Working with `cv.VideoCapture()`")
-video_capture()
-
-st.code("""
-import numpy as np
-import cv2 as cv
-cap = cv.VideoCapture(0)
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
-while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
-    # Our operations on the frame come here
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    # Display the resulting frame
-    cv.imshow('frame', gray)
-    if cv.waitKey(1) == ord('q'):
-        break
-# When everything done, release the capture
-cap.release()
-cv.destroyAllWindows()
-""", line_numbers=True)
+col1, col2,_, _ = st.columns([1,1,1,1])
+start = col1.button("Run it :green[▶️]", use_container_width=True)
+if start: 
+    video_capture(col2)
+else:
+    st.code("""
+    import numpy as np
+    import cv2 as cv
+    cap = cv.VideoCapture(0)
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
+    while True:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        # if frame is read correctly ret is True
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+        # Our operations on the frame come here
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        # Display the resulting frame
+        cv.imshow('frame', gray)
+        if cv.waitKey(1) == ord('q'):
+            break
+    # When everything done, release the capture
+    cap.release()
+    cv.destroyAllWindows()
+    """, line_numbers=True)
 
 with st.expander("Details", expanded=False):
     st.markdown("""
