134c18d38b24b4d9fa44e63501ca590df0dab45e
Zehan <zehan.kareem@gmail.com>
2024-02-06 01:39:59 +0600

    create read_image()

--- app/utils/images.py
+++ app/utils/images.py
@@ -1,11 +1,14 @@
 import cv2 as cv
 import numpy as np
 
-
+def read_image(path):
+    img = cv.imread(path)
+    return img
 def bytes_to_image(byte_string):
     # Convert the byte string to a NumPy array
     image_array = np.frombuffer(byte_string, dtype=np.uint8)
     # Decode the NumPy array to an image using OpenCV
     image = cv.imdecode(image_array, cv.IMREAD_COLOR)
     return image
+
 
