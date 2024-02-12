f9ec3fa3c0da92b699b11c6cb1558b9ebc891d3c
Zehan <zehan.kareem@gmail.com>
2024-02-12 19:22:21 +0600

    Major Update

--- app/utils/gui/drawing.py
+++ app/utils/gui/drawing.py
@@ -8,22 +8,26 @@ from utils.opencv.drawing import \
     draw_polygon,
     draw_text)
 
-# Draw Line Parameters
 def Draw_Line():
+    """
+    The `Draw_Line` function allows users to draw a line on an image by specifying the starting and
+    ending coordinates, color, and thickness of the line.
+    """
     
-    def write_code(start, end, color, thickness):
+    def write_code(start:tuple, end:tuple, color:tuple, thickness:int) -> str:
         return f"""
                     import numpy as np
                     import cv2 as cv
                     # Create a black image
                     img = np.zeros((512,512,3), np.uint8)
                     # Draw a diagonal red line with thickness of 5 px
-                    cv.line(img,{start},{end},{color[::-1]},{thickness})
+                    cv.line(img, {start}, {end}, {color[::-1]}, {thickness})
                     cv.imshow('Output', img)
         """
-    defaults=[(0, 0), (511, 511), (255, 0, 0), 5]
-    main_container = st.empty().container(border=True)
     
+    defaults=[(0, 0), (511, 511), (255, 0, 0), 5]
+    
+    main_container = st.empty().container(border=True)
     main_container.subheader("Drawing Line")
     main_container.markdown("""
                 To draw a line, you need to pass starting and ending coordinates of line. 
@@ -71,6 +75,10 @@ def Draw_Line():
 
 
 def Draw_Rectangle():
+    """
+    The `Draw_Rectangle` function allows the user to draw a rectangle on an image by specifying the
+    top-left and bottom-right coordinates, color, and thickness.
+    """
     
     def write_code(top_left, bottom_right, color, thickness):
         return f"""
@@ -143,6 +151,10 @@ def Draw_Rectangle():
         code_container.info("Example code")
             
 def Draw_Circle():
+    """
+    The `Draw_Circle` function allows users to draw a circle by specifying the center coordinates,
+    radius, color, and thickness.
+    """
     
     def write_code(center, radius, color, thickness):
         return f"""
@@ -224,10 +236,42 @@ def Draw_Circle():
         code_container.info("Example Code")
 
 def Draw_Ellipse():
+    """
+    The function `Draw_Ellipse()` allows users to draw an ellipse on an image using OpenCV in Python,
+    with customizable parameters such as center location, axes lengths, angle, start angle, end angle,
+    color, and thickness.
+    """
     
-    with st.container(border=True):
-        st.subheader("Drawing Ellipse")
-        st.markdown("""
+    def write_code( center:tuple, 
+                    axes_length:tuple,
+                    angle:int,
+                    start_angle:int,
+                    end_angle:int,
+                    color:tuple,
+                    thickness:int) -> str:
+        
+        return f"""
+            import numpy as np
+            import cv2 as cv
+            # Create a black image
+            img = np.zeros((512,512,3), np.uint8)
+            # Draw a diagonal blue line with thickness of 5 px
+            cv.ellipse(img,{center}, {axes_length}, {angle}, {start_angle}, {end_angle}, {color[::-1]}, {thickness})
+            cv.imshow('Output', img)
+        """
+        
+    
+    defaults=[(447, 63), (100, 50), 0, 0, 180, (255, 0, 0), -1]
+    
+    # The code below is creating a Python script that uses the `st` module to create a user interface
+    # for drawing an ellipse. It creates a main container with a border, adds a subheader for the
+    # "Drawing Ellipse" section, and provides a markdown explanation of how to draw an ellipse using
+    # the `cv.ellipse()` function. It also creates an image container and a code container within the
+    # main container.
+    
+    main_container = st.container(border=True)
+    main_container.subheader("Drawing Ellipse")
+    main_container.markdown("""
                     To draw the ellipse, we need to pass several arguments.
                     One argument is the center location (x,y).
                     Next argument is axes lengths (major axis length, minor axis length).
@@ -238,68 +282,106 @@ def Draw_Ellipse():
                     For more details, check the documentation of cv.ellipse().
                     Below example draws a half ellipse at the center of the image.
                     """)
-        st.sidebar.info("Change Parameters to see differences")
+    
+    image_container = main_container.container(border=True)
+    code_container = main_container.container(border=True)
+    
+    with st.sidebar:
+        
+        st.markdown("<center><h5>Parameters</h5></center>", unsafe_allow_html=True)
+        st.info("Change Parameters to see differences")
         
         with st.container(border=True):
+        
+            st.markdown("<center>Center</center>",
+                        unsafe_allow_html=True)
+            center = st.slider("`x - coordinate`",value=447,max_value=512),\
+                                st.slider("` y - coordinate`", value=63, max_value= 512)
+
+            st.markdown("<center>Axes Length</center>", unsafe_allow_html=True)
+            axes_length= st.slider("`axes_length - major`", value=100, max_value=512),\
+                        st.slider("`axes_length - minor`", value=50, max_value=512)
             
-            with st.sidebar.container(border=True):
+            st.markdown("<center>Angle</center>", unsafe_allow_html=True)
+            angle = st.slider("`angle`", value = 0, max_value=360)
             
-                st.markdown("<center>Center</center>",
-                            unsafe_allow_html=True)
-                center_x, center_y = st.slider("`x - coordinate`",value=447,max_value=512),\
-                                    st.slider("` y - coordinate`", value=63, max_value= 512)
-                center = (center_x, center_y)
-                
-                st.markdown("<center>Axes Length</center>", unsafe_allow_html=True)
-                axes_length= st.slider("`axes_length - major`", value=100, max_value=512),\
-                            st.slider("`axes_length - minor`", value=50, max_value=512)
-                
-                st.markdown("<center>Angle</center>", unsafe_allow_html=True)
-                angle = st.slider("`angle`", value = 0, max_value=360)
-                
-                st.markdown("<center>Start Angle</center>", unsafe_allow_html=True)
-                start_angle = st.slider("`start_angle`", value = 0, max_value=360)
-                
-                st.markdown("<center>End Angle</center>", unsafe_allow_html=True)
-                end_angle = st.slider("`end_angle`", value = 180, max_value=360)
-                
-                st.markdown("<center>Color</center>", unsafe_allow_html=True)
-                color = st.color_picker("Pick a color",value="#ff0000", label_visibility="hidden")
-                color = ImageColor.getcolor(f'{color}','RGB')
-                
-                st.markdown("<center>Thickness</center>", unsafe_allow_html=True)
-                thickness=st.slider("Thickness",value=2, min_value=-1,
-                                            max_value=10, label_visibility="hidden")
-                
-                if st.checkbox("Fill Circle", value=True): thickness=-1
+            st.markdown("<center>Start Angle</center>", unsafe_allow_html=True)
+            start_angle = st.slider("`start_angle`", value = 0, max_value=360)
             
-            st.markdown("<center>Output</center>", unsafe_allow_html=True)
-            st.image(draw_ellipse( center, 
-                                    axes_length,
-                                    angle,
-                                    start_angle,
-                                    end_angle,
-                                    color,
-                                    thickness ),
-                       'Draw Ellipse',
-                       use_column_width=True)
+            st.markdown("<center>End Angle</center>", unsafe_allow_html=True)
+            end_angle = st.slider("`end_angle`", value = 180, max_value=360)
             
-                
-        with st.container(border=True):
-            st.markdown("### Code")
-            st.code(f"""
-            import numpy as np
-            import cv2 as cv
-            # Create a black image
-            img = np.zeros((512,512,3), np.uint8)
-            # Draw a diagonal blue line with thickness of 5 px
-            cv.ellipse(img,{center}, {axes_length}, {angle}, {start_angle}, {end_angle}, {color[::-1]}, {thickness})
-            cv.imshow('Output', img)
-        """)
+            st.markdown("<center>Color</center>", unsafe_allow_html=True)
+            color = st.color_picker("Pick a color",value="#ff0000", label_visibility="hidden")
+            color = ImageColor.getcolor(f'{color}','RGB')
+            
+            st.markdown("<center>Thickness</center>", unsafe_allow_html=True)
+            
+            thickness=st.slider("Thickness",value=-1, min_value=-1,
+                                        max_value=10, label_visibility="hidden")
+            
+            # The code is checking the value of the variable `thickness`. If `thickness` is less than
+            # 0, it assigns `True` to the variable `val`. Otherwise, it assigns `False` to `val`.
+            val = True if thickness<0 else False
+            
+            if st.checkbox("Fill? ", value=val): thickness=-1
+    
+    image_container.subheader("Output")
+    code_container.subheader("Code")
+   
+    # The code below  is checking if the parameters for drawing an ellipse (center, axes_length, angle,
+    # start_angle, end_angle, color, thickness) are equal to the default values. If they are not equal
+    # to the defaults, it will draw an ellipse with the given parameters and display the output image.
+    # It will also display the code used to draw the ellipse. If the parameters are equal to the
+    # defaults, it will draw an example ellipse with the default parameters and display the example
+    # output image. It will also display the example code used to draw the ellipse.
+    
+    if [center, 
+        axes_length,
+        angle,
+        start_angle,
+        end_angle,
+        color,
+        thickness] != defaults:
+        
+        image_container.image(draw_ellipse( center, 
+                            axes_length,
+                            angle,
+                            start_angle,
+                            end_angle,
+                            color,
+                            thickness ),
+                'Draw Ellipse',
+                use_column_width=True)
+        image_container.success("Your Output")
+        
+        code_container.success("Your Code")
+        code_container.code(write_code(center, 
+                            axes_length,
+                            angle,
+                            start_angle,
+                            end_angle,
+                            color,
+                            thickness))
+        
+    else:
+        image_container.image(draw_ellipse(*defaults),
+        'Draw Ellipse',
+        use_column_width=True)
+        image_container.info("Example Output")
+        
+        code_container.info("Example Code")
+        code_container.code(write_code(center, 
+                            axes_length,
+                            angle,
+                            start_angle,
+                            end_angle,
+                            color,
+                            thickness))
 
 def Draw_Polygon():
     
-    def write_code(pts=[[10,5],[20,30],[70,20],[50,10]], join=True, color=(255,255,255)):
+    def write_code(pts, join, color):
         return f"""
                 import numpy as np
                 import cv2 as cv
@@ -310,60 +392,64 @@ def Draw_Polygon():
                 cv.polylines(img,[pts], {join}, {color})
                 cv.imshow('Output', img)
                 """
-    with st.container(border=True):
-        st.subheader("Drawing Polygons")
-        st.markdown("""
-                    To draw a polygon, first you need coordinates of vertices.
-                    Make those points into an array of shape ROWSx1x2 where ROWS
-                    are number of vertices and it should be of type int32. 
-                    Here we draw a small polygon of with four vertices in yellow color.
-                    """)
-                
-        image_container=st.empty()
-        code_container = st.empty().container(border=True)
-        st.markdown("""
-                    #### Note
-                    > If third argument is False, you will get a polylines
-                    joining all the points, not a closed shape.
-                    `cv.polylines()` can be used to draw multiple lines. 
-                    Just create a list of all the lines you want to draw 
-                    and pass it to the function.
-                    All lines will be drawn individually. 
-                    It is a much better and faster way to draw a group of lines
-                    than calling cv.line() for each line.
-                    """)
     
-    live = False
-    points = st.sidebar.number_input("How many points for your Polygon?", value=0)
-    st.sidebar.info("Change Parameters to see differences")
+    defaults = [[[10,5],[20,30],[70,20],[50,10]], True, (255,255,255)]
     
-    st.sidebar.subheader("Parameters")
-    with st.sidebar.container(border=True):
-        
+    main_container = st.empty().container(border=True)
+    
+    main_container.subheader("Drawing Polygons")
+    main_container.markdown("""
+                To draw a polygon, first you need coordinates of vertices.
+                Make those points into an array of shape ROWSx1x2 where ROWS
+                are number of vertices and it should be of type int32. 
+                Here we draw a small polygon of with four vertices in yellow color.
+                """)
+    
+    image_container = main_container.container(border=True)
+    code_container = main_container.container(border=True)
+
+    main_container.markdown("""
+                #### Note
+                > If third argument is False, you will get a polylines
+                joining all the points, not a closed shape.
+                `cv.polylines()` can be used to draw multiple lines. 
+                Just create a list of all the lines you want to draw 
+                and pass it to the function.
+                All lines will be drawn individually. 
+                It is a much better and faster way to draw a group of lines
+                than calling cv.line() for each line.
+                """)
+    
+    with st.sidebar:
+        st.markdown("<center> <h5> Parameters </h5> </center>", unsafe_allow_html=True)
+        st.info("Change Parameters to see differences")
+        points = st.sidebar.number_input("How many points for your Polygon?", value=0)
         pts = []
+        with st.container(border=True):
+            if points:
+                for i in range(points):
+                    st.markdown(f"Coordinates for point : `{i+1}`")
+                    x = st.slider(f"$x_{i+1}$:", key=f"x{i}")
+                    y = st.slider(f"$y_{i+1}$:", key=f"y{i}")
+                    pts.append([x, y])
+                    
+            join = st.checkbox("Join?", value=True)
+            color = st.color_picker("Color:", value="#00ffff")
+            color = ImageColor.getcolor(f'{color}','RGB')
         
-        if points:
-            live=True
-            for i in range(points):
-                st.markdown(f"Coordinates for point : `{i+1}`")
-                x = st.slider(f"$x_{i+1}$:", key=f"x{i}")
-                y = st.slider(f"$y_{i+1}$:", key=f"y{i}")
-                pts.append([x, y])
-                
-        join = st.checkbox("Join?", value=True)
-        color = st.color_picker("Color:", value="#00ffff")
-        color = ImageColor.getcolor(f'{color}','RGB')
-        
-    with image_container.container(border=True):
-        st.markdown("<center> Output </center>", unsafe_allow_html=True)
-        if live: 
-            st.image(draw_polygon(pts, join, color), caption="Drawing Polygon", use_column_width=True)
-            code_container.markdown("### Code")
-            code_container.code(write_code(pts, join, color))
-        else:
-            st.image(draw_polygon(), caption="Drawing Polygon", use_column_width=True)
-            code_container.markdown("### Code")
-            code_container.code(write_code())
+    image_container.subheader("Output")
+    code_container.subheader("Code")
+    
+    if points: 
+        image_container.image(draw_polygon(pts, join, color), caption="Drawing Polygon", use_column_width=True)
+        image_container.success("Your Output")
+        code_container.code(write_code(pts, join, color))
+        code_container.success("Your Code")
+    else:
+        image_container.image(draw_polygon(), caption="Drawing Polygon", use_column_width=True)
+        image_container.info("Example Output")
+        code_container.code(write_code(*defaults))
+        code_container.info("Example Code")
         
 
 def Draw_Text():
