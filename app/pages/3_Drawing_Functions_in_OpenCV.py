d2473e3ba7536f8918b4e799c4c972e7b7a81548
Zehan <zehan.kareem@gmail.com>
2024-02-12 20:06:56 +0600

    Add option - 'Introduction'

--- app/pages/3_Drawing_Functions_in_OpenCV.py
+++ app/pages/3_Drawing_Functions_in_OpenCV.py
@@ -10,30 +10,14 @@ from utils.gui.drawing import (
 )
 
 if __name__ == "__main__":
+    
     st.set_page_config(page_icon="app\\assets\\OpenCV_Logo_with_text.png",
                        page_title="Drawing Geometric Shapes with OpenCV")
-    st.markdown("""
-                ## Drawing Geometric Shapes with OpenCV 📐📏 🟥🔴🔺
-                
-                ## Goal
-                Learn to draw different geometric shapes with OpenCV. The functions covered include 
-                `cv.line()`, `cv.circle()`, `cv.rectangle()`, `cv.ellipse()`, `cv.putText()`, and more.
-
-                ## Code
-                In all the above functions, you will find common arguments as given below:
-
-                - `img`: The image where you want to draw the shapes.
-                - `color`: Color of the shape. For BGR, pass it as a tuple, e.g., (255, 0, 0) for blue. 
-                For grayscale, just pass the scalar value.
-                - `thickness`: Thickness of the line or circle, etc. 
-                If -1 is passed for closed figures like circles, it will fill the shape. The default thickness is 1.
-                - `lineType`: Type of line, whether 8-connected, anti-aliased line, etc. By default, it is 8-connected. 
-                `cv.LINE_AA` gives an anti-aliased line, which looks great for curves.
-    """)
-
+    
     st.sidebar.subheader("Drawing Options")
     options = st.sidebar.selectbox(label="Select:",
-                                   options=["Drawing Line",
+                                   options=["Introduction",
+                                            "Drawing Line",
                                             "Drawing Rectangle",
                                             "Drawing Circle",
                                             "Draw Ellipse",
@@ -41,6 +25,26 @@ if __name__ == "__main__":
                                             "Adding Texts"],
                                    label_visibility="collapsed")
     
+    if options == "Introduction":
+        st.markdown("""
+                    ## Drawing Geometric Shapes with OpenCV 📐📏 🟥🔴🔺
+                    
+                    ## Goal
+                    Learn to draw different geometric shapes with OpenCV. The functions covered include 
+                    `cv.line()`, `cv.circle()`, `cv.rectangle()`, `cv.ellipse()`, `cv.putText()`, and more.
+
+                    ## Code
+                    In all the above functions, you will find common arguments as given below:
+
+                    - `img`: The image where you want to draw the shapes.
+                    - `color`: Color of the shape. For BGR, pass it as a tuple, e.g., (255, 0, 0) for blue. 
+                    For grayscale, just pass the scalar value.
+                    - `thickness`: Thickness of the line or circle, etc. 
+                    If -1 is passed for closed figures like circles, it will fill the shape. The default thickness is 1.
+                    - `lineType`: Type of line, whether 8-connected, anti-aliased line, etc. By default, it is 8-connected. 
+                    `cv.LINE_AA` gives an anti-aliased line, which looks great for curves.
+        """)
+        
     if options == "Drawing Line":
         Draw_Line()
         
@@ -58,3 +62,4 @@ if __name__ == "__main__":
         
     if options == "Adding Texts":
         Draw_Text()
+        
