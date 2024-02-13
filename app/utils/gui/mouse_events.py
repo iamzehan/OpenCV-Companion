474edf38889e13e076bea587925386e6dbf2a452
Zehan <zehan.kareem@gmail.com>
2024-02-14 01:47:03 +0600

    Advanced_Demo() gui added

--- app/utils/gui/mouse_events.py
+++ app/utils/gui/mouse_events.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import cv2 as cv
 
-
 def Simple_Demo():
     st.markdown("""<h2> Simple Demo<h2>
                         <p> 
@@ -52,7 +51,62 @@ while(1):
 cv.destroyAllWindows()
             """)
     with st.expander("Output:"):
-        st.image("app/assets/GIFs/mouse_events.gif", use_column_width=True)
+        st.image("app/assets/GIFs/mouse_events.gif", caption='image', use_column_width=True)
 
 def Advanced_Demo():
-    pass
+    st.markdown("## More Advanced Demo")
+    st.markdown("""
+                Now we go for a much better application. In this, we draw either rectangles 
+                or circles (depending on the mode we select) by dragging the mouse like we do
+                in Paint application. So our mouse callback function has two parts, one to 
+                draw rectangle and other to draw the circles. 
+                This specific example will be really helpful in creating and understanding 
+                some interactive applications like object tracking, image segmentation etc.
+                """)
+    st.code("""
+            import numpy as np
+            import cv2 as cv
+            drawing = False # true if mouse is pressed
+            mode = True # if True, draw rectangle. Press 'm' to toggle to curve
+            ix,iy = -1,-1
+            # mouse callback function
+            def draw_circle(event,x,y,flags,param):
+                global ix,iy,drawing,mode
+                if event == cv.EVENT_LBUTTONDOWN:
+                    drawing = True
+                    ix,iy = x,y
+                elif event == cv.EVENT_MOUSEMOVE:
+                    if drawing == True:
+                        if mode == True:
+                            cv.rectangle(img,(ix,iy),(x,y),(0,255,0),-1)
+                        else:
+                            cv.circle(img,(x,y),5,(0,0,255),-1)
+                elif event == cv.EVENT_LBUTTONUP:
+                    drawing = False
+                    if mode == True:
+                        cv.rectangle(img,(ix,iy),(x,y),(0,255,0),-1)
+                    else:
+                        cv.circle(img,(x,y),5,(0,0,255),-1)
+            """)
+    with st.expander("Output: "):
+        st.image("app/assets/GIFs/advanced_mouse_events.gif", use_column_width=True)
+    
+    st.markdown("""
+                Next we have to bind this mouse callback function to OpenCV window.
+                In the main loop, we should set a keyboard binding for key 'm' to 
+                toggle between rectangle and circle.
+                """)
+    st.code("""
+            img = np.zeros((512,512,3), np.uint8)
+            cv.namedWindow('image')
+            cv.setMouseCallback('image',draw_circle)
+            while(1):
+                cv.imshow('image',img)
+                k = cv.waitKey(1) & 0xFF
+                if k == ord('m'):
+                    mode = not mode
+                elif k == 27:
+                    break
+            cv.destroyAllWindows()
+            """)
+    
