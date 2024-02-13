788fc9a2233435d96cda45fc4bbc1b66eb5d21a8
Zehan <zehan.kareem@gmail.com>
2024-02-14 01:18:28 +0600

    Path for output GIF changed

--- app/utils/gui/mouse_events.py
+++ app/utils/gui/mouse_events.py
@@ -52,7 +52,7 @@ while(1):
 cv.destroyAllWindows()
             """)
     with st.expander("Output:"):
-        st.image("app\\assets\\GIFs\\mouse_events.gif", use_column_width=True)
+        st.image("app/assets/GIFs/mouse_events.gif", use_column_width=True)
 
 def Advanced_Demo():
     pass
