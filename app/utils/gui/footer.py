e08d5788a02bb20b0a60400c4d97f1af6182bf8b
Zehan <zehan.kareem@gmail.com>
2024-02-15 19:12:46 +0600

    Adding version to footer

--- app/utils/gui/footer.py
+++ app/utils/gui/footer.py
@@ -1,6 +1,26 @@
 import json
 import streamlit as st
 import datetime as dt
+import requests
+
+def get_latest_release():
+    repo_owner = "iamzehan"
+    repo_name = "OpenCV-Companion"
+
+    # GitHub API endpoint for releases
+    api_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/releases/latest"
+
+    # Fetch the latest release information
+    response = requests.get(api_url)
+    latest_release_data = response.json()
+    # Extract the latest release version and URL
+    try:
+        latest_release_version = latest_release_data["tag_name"]
+        latest_release_url = latest_release_data["html_url"]
+    except:
+        latest_release_version = ""
+        latest_release_url = "#"
+    return f"{latest_release_version}"
 
 def footer():
     config = json.load(open("config.json"))
@@ -38,12 +58,16 @@ def footer():
             }}
             </style>
             <footer>
+                <p style="color:grey"> 
                 <a href="{config["Linkedin"]}" target="_blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="{config["Linkedin"]}" height="20" width="30" /></a>
                 <a href="{config["Github"]}" target="_blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/github.svg" height="20" width="30" /></a>
                 <a href="https://mail.google.com/mail/?view=cm&fs=1&to=ziaul.karim497@gmail.com" target="_blank"><img align="center" src="https://upload.wikimedia.org/wikipedia/commons/7/7e/Gmail_icon_%282020%29.svg" height="15" width="30" /></a>
                 <br>
+                {get_latest_release()}
+                <br>
                 &copy; {dt.datetime.now().year} Made by - <a href= "https://ziaulkarim.netlify.app/" target="_blank"> Md. Ziaul Karim </a>
             </footer>
+            
             """,
             unsafe_allow_html=True
         )
