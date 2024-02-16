38f9ff67951cd5524d40881afaedc2c7e1490ff3
Zehan <zehan.kareem@gmail.com>
2024-02-16 20:19:10 +0600

    Using GUIFeatures Class

--- app/pages/1_Getting_Started_with_Images.py
+++ app/pages/1_Getting_Started_with_Images.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from utils.gui.footer import footer
-from utils.gui.images import Read_and_Show_Image
+from utils.gui.images import GUIFeatures
 
 if __name__ == "__main__":
     
@@ -18,20 +18,7 @@ if __name__ == "__main__":
                 """,
                 unsafe_allow_html=True)
     
-    # File and name handling
-    img_file_name = 'Lenna.png'
-    render = st.empty().container()
-    render.subheader("Code")
-    st.sidebar.info("Upload an image to see changes")
-    img_file = st.sidebar.file_uploader("Upload an Image to see how the code changes:", type=["PNG","JPG"], label_visibility="collapsed")
-    
-    # Checks if a File has been uploaded
-    if img_file:
-        # extracting name img_file object of the Upload class
-        img_file_name = img_file.name
-        # rendition of the whole view
-        Read_and_Show_Image(img_file, img_file_name, render, upload=True)
-    else:
-        Read_and_Show_Image(img_file, img_file_name, render)
+    guiFeat = GUIFeatures()
+    guiFeat.Read_and_Show_Image()
     
     footer()
