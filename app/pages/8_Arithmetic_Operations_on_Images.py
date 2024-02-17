ebb766b27b7c92223d45235348597065114d9df0
Zehan <zehan.kareem@gmail.com>
2024-02-18 00:56:26 +0600

    Add options - 'Image Addition'

--- app/pages/8_Arithmetic_Operations_on_Images.py
+++ app/pages/8_Arithmetic_Operations_on_Images.py
@@ -1,15 +1,16 @@
 import streamlit as st 
 from utils.gui.footer import footer
+from utils.gui.images import ArithmeticOperations
 if __name__ == '__main__':
     options = st.sidebar.selectbox(label="Navigate: ",options=["Introduction",
                                                                "Image Addition",
                                                                "Image Blending",
                                                                "Bitwise Operations"],
                                    label_visibility="collapsed")
-    
+    arithmeticOps = ArithmeticOperations() 
+    st.markdown("# Arithmetic Operations on Images")
     if options == "Introduction":
         st.markdown("""
-                    # Arithmetic Operations on Images
                     ## Goal
                     - Learn several arithmetic operations on images like 
                     addition, subtraction, bitwise operations etc.
@@ -18,7 +19,8 @@ if __name__ == '__main__':
                     """)
         
     if options == "Image Addition":
-        pass
+        arithmeticOps.side_bar(multiple=True)
+        arithmeticOps.Image_Addition()
     if options == "Image Blending":
         pass
     if options == "Bitwise Operations":
