4e77f6536026ea53215dfa0d88b34f86a1508652
Zehan <zehan.kareem@gmail.com>
2024-02-18 03:07:07 +0600

    Some changes in footer

--- app/utils/gui/footer.py
+++ app/utils/gui/footer.py
@@ -1,14 +1,8 @@
-import json
+import toml
 import streamlit as st
 import datetime as dt
 import requests
-import toml
 
-def update_config(data):
-    # Write data to the JSON file
-    with open('.streamlit/secrets.toml', 'w') as toml_file:
-        toml.dump(data, toml_file)
-        
 def get_latest_release(config):
 
     # GitHub API endpoint for releases
@@ -19,12 +13,9 @@ def get_latest_release(config):
         response = requests.get(api_url)
         latest_release_data = response.json()
         latest_release_version = latest_release_data["tag_name"]
-        if latest_release_version!=config["footer"]["latest_release_url"]:
-            config["footer"]["latest_release"]=latest_release_version
-            update_config(config)
-            return latest_release_version
+        return latest_release_version
     except:
-        return config["footer"]["latest_release"]
+        return "Latest"
 
 def footer():
     config = toml.load(open(".streamlit/secrets.toml", 'r'))
@@ -35,7 +26,7 @@ def footer():
             
             <style>
                 footer {{
-                    color: #fff;
+                    color: grey;
                     text-align: center;
                     padding: 1rem;
                     position: relative;
@@ -47,8 +38,10 @@ def footer():
                     text-decoration: none;
                     font-weight: bold;
                 }}
+                
                 a:hover {{
                     text-decoration: underline;
+                    color: #34eb5b;
                 }}
                 
                 .blank-div {{
@@ -61,16 +54,37 @@ def footer():
                 color: white; /* set the text color to white for better visibility */
                 font-size: 24px;
             }}
+            
+            .Label--success {{
+                    border-color: #34eb5b;
+                    color: #34eb5b;
+            }}
+
+            .Label, .label {{
+                border: 1px solid #34eb5b;
+                border-radius: 1em;
+                display: inline-block;
+                font-size: var(--text-body-size-small, 0.75rem);
+                font-weight: var(--base-text-weight-medium, 500);
+                line-height: 18px;
+                padding: 0 7px;
+                white-space: nowrap;
+            }}
+            
             </style>
             <footer>
                 <p style="color:grey"> 
                 <a href="{config["footer"]["linkedin"]}" target="_blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="{config["footer"]["linkedin"]}" height="20" width="30" /></a>
                 <a href="{config["footer"]["github"]}" target="_blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/github.svg" height="20" width="30" /></a>
                 <a href="{config["footer"]["mail"]}" target="_blank"><img align="center" src="https://upload.wikimedia.org/wikipedia/commons/7/7e/Gmail_icon_%282020%29.svg" height="15" width="30" /></a>
-                <br>
-                {latest_release_version}
-                <br>
-                &copy; {dt.datetime.now().year} Made by - <a href= "{config["footer"]["website"]}" target="_blank"> Md. Ziaul Karim </a>
+                <div>
+                    <span title="Label: Latest" data-view-component="true" class="Label Label--success flex-shrink-0">
+                        {latest_release_version}
+                    </span>      
+                </div>
+                <div>
+                &copy; {dt.datetime.now().year} Made by - <a href= "{config["footer"]["website"]}" target="_blank" style='color: #34eb5b'> Md. Ziaul Karim </a>
+                </div>
             </footer>
             
             """,
