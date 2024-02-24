fcd1d5f11743f9267e9b77b924287f7036c32fb8
Zehan <zehan.kareem@gmail.com>
2024-02-24 15:54:20 +0600

    Complete page Core Operations

---
+++ app/pages/6_Core_Operations_🔴.py
@@ -0,0 +1,36 @@
+import streamlit as st 
+from utils.gui.footer import footer
+
+st.title("Core Operations in OpenCV")
+
+with st.container(border=True):
+    st.page_link("pages/7_Basic_Operations_on_Images.py", label="👉 Basic Operations on Images", use_container_width=True)
+    col1, col2, col3 = st.columns([2,8,2])
+    col1.image("https://opencv24-python-tutorials.readthedocs.io/en/latest/_images/pixel_ops.jpg")
+    col2.markdown("Learn to read and edit pixel values, working with image ROI and other basic operations.")
+    col3.page_link("pages/7_Basic_Operations_on_Images.py", label="**Learn** ➡️", use_container_width=True)
+
+with st.container(border=True):
+    st.page_link("pages/8_Arithmetic_Operations_on_Images.py", label="👉 Arithmetic Operations on Images")
+    col1, col2, col3 = st.columns([2,8,2])
+    col1.image("https://opencv24-python-tutorials.readthedocs.io/en/latest/_images/image_arithmetic.jpg")
+    col2.markdown("Perform arithmetic operations on images")
+    col3.page_link("pages/8_Arithmetic_Operations_on_Images.py", label="**Learn** ➡️")
+
+with st.container(border=True):
+    st.page_link("pages/9_Performance_Measurement_and_Improvement_Techniques.py", label=" 👉 Performance Measurement and Improvement Techniques")
+    col1, col2, col3 = st.columns([2,8,2])
+    col1.image("https://opencv24-python-tutorials.readthedocs.io/en/latest/_images/speed.jpg")
+    col2.markdown("Getting a solution is important. But getting it in the fastest way is more important. Learn to check the speed of your code, optimize the code etc.")
+    col3.page_link("pages/9_Performance_Measurement_and_Improvement_Techniques.py", label="**Learn** ➡️")
+    
+with st.container(border=True):
+    st.page_link("pages/10_Mathematical_Tools_in_OpenCV.py", label="👉 Mathematical Tools in OpenCV", use_container_width=True)
+    col1, col2, col3 = st.columns([2,8,2])
+    col1.image("https://opencv24-python-tutorials.readthedocs.io/en/latest/_images/maths_tools.jpg")
+    col2.markdown("Learn some of the mathematical tools provided by OpenCV like PCA, SVD etc.")
+    col3.page_link("pages/10_Mathematical_Tools_in_OpenCV.py", label="**Learn ➡️**")
+    
+col1, col2, col3 = st.columns([2,8,2])
+col3.page_link("pages/7_Basic_Operations_on_Images.py", label="Next ➡️", use_container_width=True)
+footer()
