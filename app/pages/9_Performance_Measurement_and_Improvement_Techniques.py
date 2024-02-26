808b3ec90eee7542c72c9fdc0b33d3beb5bbd9d5
Zehan <zehan.kareem@gmail.com>
2024-02-26 20:16:49 +0600

    Added Select Options

--- app/pages/9_Performance_Measurement_and_Improvement_Techniques.py
+++ app/pages/9_Performance_Measurement_and_Improvement_Techniques.py
@@ -3,9 +3,55 @@ from utils.gui.footer import footer
 from  utils.gui.menu import core_operations_menu
 
 def main():
-    pass
+    st.markdown("""
+                <center> 
+                    <h2> Performance Measurement and Improvement Techniques 
+                    </h2> 
+                </center>
+                
+                """, unsafe_allow_html=True)
+    
+    options = st.sidebar.selectbox("Select: ", options=['Introduction',
+                                                'Measuring Performance with OpenCV',
+                                                'Default Optimization',
+                                                'Measuring Performance in IPython',
+                                                'More IPython magic Commands',
+                                                'Performance Optimization Techniques'],
+                           label_visibility="collapsed")
 
+    if options == "Introduction":
+        st.markdown("""
+                    ### Goal
+                    In image processing, since you are dealing with large
+                    number of operations per second, it is mandatory that 
+                    your code is not only providing the correct solution, 
+                    but also in the fastest manner. So in this chapter, 
+                    you will learn
+
+                    - To measure the performance of your code.
+                    - Some tips to improve the performance of your code.
+                    - You will see these functions : `cv2.getTickCount`, 
+                    `cv2.getTickFrequency` etc.
+                    """)
+
+    elif options == "Measuring Performance with OpenCV":
+        st.subheader("Measuring Performance with OpenCV")
+       
+    elif options == "Default Optimization":
+        st.subheader("Default Optimization")
+        
+    elif options == "Measuring Performance in IPython":
+        st.subheader("Measuring Performance in IPython")
+        
+    elif options == "More IPython magic Commands":
+        st.subheader("More IPython magic Commands")
+        
+    elif options == "Performance Optimization Techniques":
+        st.subheader("Performance Optimization Techniques")
+        
 if __name__ == '__main__':
+    st.set_page_config(page_title="Performance Measurement & Improvement",
+                       page_icon="app/assets/Images/OpenCV_Logo_with_text.png")
     core_operations_menu()
     main()
     footer()
