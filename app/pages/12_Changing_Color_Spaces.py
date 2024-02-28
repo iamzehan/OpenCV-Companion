780156a7f7dd18b9fea9e664204cb63d84411f72
Zehan <zehan.kareem@gmail.com>
2024-02-29 00:45:44 +0600

    Changing colors page added

---
+++ app/pages/12_Changing_Color_Spaces.py
@@ -0,0 +1,36 @@
+import streamlit as st 
+from utils.gui.menu import menu
+from utils.gui.footer import footer
+from utils.gui.images import ChangingColorSpace
+
+def main():
+    ccs = ChangingColorSpace()
+    with st.sidebar.container(border=True):
+        st.subheader("Topics")
+        options = st.radio("Select: ", options=['Introduction',
+                                                'Changing Color-Space',
+                                                'Object-Tracking',
+                                                ], label_visibility="collapsed")
+    st.title("Image Processing in OpenCV")
+    if options == "Introduction":
+        st.markdown("""
+                    ## Goal
+                    - In this tutorial, you will learn how to convert images from one color-space to another,
+                    like BGR $$\leftrightarrow$$ Gray, BGR $$\leftrightarrow$$ HSV etc.
+                    - In addition to that, we will create an application which extracts a colored object in a video
+                    - You will learn following functions : `cv2.cvtColor()`, `cv2.inRange()` etc.
+                    """)
+    if options == "Changing Color-Space":
+        st.subheader("Changing Color-Space")
+        ccs.Changing_Colorspace()
+    
+    if options == "Object-Tracking":
+        st.subheader("Object-Tracking")
+        ccs.side_bar()
+        ccs.Object_Tracking()
+
+if __name__ == '__main__':
+    st.set_page_config("Changing Colorspaces", page_icon='app/assets/Images/OpenCV_Logo_with_text.png')
+    menu()
+    main()
+    footer()
