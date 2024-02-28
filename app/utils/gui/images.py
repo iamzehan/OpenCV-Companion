77c6d41fa8d64a879efa96bab1def235f2662272
Zehan <zehan.kareem@gmail.com>
2024-02-28 18:12:36 +0600

    Added the rest of the pages

--- app/utils/gui/images.py
+++ app/utils/gui/images.py
@@ -917,10 +917,60 @@ class PerformanceMeasurement(CommonComponents):
                 But, there can be exceptions, 
                 especially when Numpy works with views instead of copies.
                 """)
+        
     def Ipython_Magic_Commands(self):
-        pass
+        st.markdown("""
+                    There are several other magic commands to measure the performance,
+                    profiling, line profiling, memory measurement etc. 
+                    They all are well documented. So only links to those docs
+                    are provided here. 
+                    Interested readers are recommended to try them out.
+                    """)
     
     def Performance_Optimization_Techniques(self):
-        pass
+        st.markdown("""
+                    There are several techniques and coding methods to exploit maximum performance of Python and Numpy. 
+                    Only relevant ones are noted here and links are given to important sources. 
+                    The main thing to be noted here is that, first try to implement the algorithm in a simple manner. 
+                    Once it is working, profile it, find the bottlenecks and optimize them.
+
+                    1. Avoid using loops in Python as far as possible, especially double/triple loops etc. 
+                    They are inherently slow.
+                    2. [Vectorize](https://www.geeksforgeeks.org/vectorization-in-python/) the algorithm/code to the maximum possible extent because Numpy and OpenCV 
+                    are optimized for vector operations.
+                    3. Exploit the [cache coherence](https://www.geeksforgeeks.org/cache-coherence/).
+                    4. Never make copies of array unless it is needed. 
+                    Try to use views instead. Array copying is a costly operation.
+                    Even after doing all these operations, if your code is still slow, or use of large loops are inevitable, use additional libraries like Cython to make it faster.
+                    """)
+        with st.expander("Using array view", expanded=False):
+            st.code("""
+                    from numpy import array
+
+                    # creating an array
+                    my_array = array([1, 2, 3, 4, 5])
+
+                    # using the array.view() method
+                    new_array = my_array.view()
+
+                    # changing the 0 index of the array
+                    new_array[0] = 9
+                    print(my_array)
+                    print(new_array)
+                    """)
+        
+        st.markdown("""
+                    Additional Resources
+                    1. [Python Optimization Techniques](http://wiki.python.org/moin/PythonSpeed/PerformanceTips)
+                    2. Scipy Lecture Notes - [Advanced Numpy](http://scipy-lectures.github.io/advanced/advanced_numpy/index.html#advanced-numpy)
+                    3. [Timing and Profiling in IPython](http://pynash.org/2013/03/06/timing-and-profiling.html)
+                    """)
+        
+        st.info("""
+                ⚠️ **Note**
+                > We are going to cover these topics with examples in the next updates.
+                As of now, the official documents conclude like this.
+                """)
+        
     
     
