f1dccd21a293bea7eead9bc8fd0d3c8c51ce45d9
Zehan <zehan.kareem@gmail.com>
2024-02-29 00:43:39 +0600

    Added Changing Colors class

--- app/utils/gui/images.py
+++ app/utils/gui/images.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from PIL import ImageColor
 from utils.opencv.images import (
+    blank_image,
     bytes_to_image,
     read_image,
     load_by_pixels,
@@ -13,7 +14,10 @@ from utils.opencv.images import (
     make_borders,
     add_two_img,
     bitwise_ops,
-    performance_measure
+    performance_measure,
+    colorspace_flags,
+    object_tracking,
+    find_hsv_values
     )
 
 # Getting Started with Images (Page - 2)
@@ -972,5 +976,210 @@ class PerformanceMeasurement(CommonComponents):
                 As of now, the official documents conclude like this.
                 """)
         
+class ImageProcessing(CommonComponents):
+    def __init__(self):
+            self.img = read_image("app/assets/Images/OpenCV_Logo_with_text.png") 
+            self.img_file=None
+            self.img_file_name = 'OpenCV_Logo_with_text.png'
     
+class ChangingColorSpace(ImageProcessing):
     
+    def __init__(self):
+        self.img = read_image("app/assets/Images/OpenCV_Logo_with_text.png") 
+        self.img_file=None
+        self.img_file_name = 'OpenCV_Logo_with_text.png'
+    
+    def Changing_Colorspace(self):
+        st.markdown("""
+                    There are more than 150 color-space conversion methods available in OpenCV. But we will look into only two which are most widely used ones, BGR \leftrightarrow Gray and BGR \leftrightarrow HSV.
+
+                    For color conversion, we use the function `cv2.cvtColor(input_image, flag)` where flag determines the type of conversion.
+
+                    For BGR $$\\rightarrow$$ Gray conversion we use the flags `cv2.COLOR_BGR2GRAY`. Similarly for BGR $$\\rightarrow$$ HSV, we use the flag `cv2.COLOR_BGR2HSV`. To get other flags, just run following commands in your Python terminal :
+                    """)
+        
+        st.code("""
+                >>> import cv2
+                >>> flags = [i for i in dir(cv2) if i.startswith('COLOR_')]
+                >>> print flags
+                """)
+        
+        with st.expander("Output", expanded=False):
+            st.write("Click ':violet[▶️]' to see details")
+            st.write(colorspace_flags())
+        
+        st.info("""
+                ⚠️ **Note:**
+                > For HSV, Hue range is [0,179], Saturation range is
+                [0,255] and Value range is [0,255]. Different softwares
+                use different scales. So if you are comparing OpenCV 
+                values with them, you need to normalize these ranges.
+                """)
+    
+    def Object_Tracking(self):
+        st.markdown("""
+                    Now we know how to convert BGR image to HSV, we can use this to extract a colored object. In HSV, it is more easier to represent a color than RGB color-space. In our application, we will try to extract a blue colored object. So here is the method:
+
+                    - Take each frame of the video
+                    - Convert from BGR to HSV color-space
+                    - We threshold the HSV image for a range of blue color
+                    - Now extract the blue object alone, we can do whatever on that image we want.
+
+                    Below is the code which are commented in detail :
+                    """)
+        
+        colorspaces = {
+            
+            "blue" : [[110,50,50], 
+                      [130,255,255]],
+            "green" : [[40, 40, 40],
+                       [80, 255, 255]],
+            "red" : [[0, 100, 100],
+                     [10, 255, 255]]
+            
+        }
+        
+        colorspace = st.selectbox(label="Choose colorspace", options=["blue",
+                                                                      "green",
+                                                                      "red"])
+        st.code(f"""
+                import cv2
+                import numpy as np
+
+                cap = cv2.VideoCapture(0)
+
+                while(1):
+
+                    # Take each frame
+                    _, frame = cap.read()
+
+                    # Convert BGR to HSV
+                    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+                    # define range of blue color in HSV
+                    lower_{colorspace} = np.array({colorspaces[colorspace][0]})
+                    upper_{colorspace} = np.array({colorspaces[colorspace][1]})
+
+                    # Threshold the HSV image to get only blue colors
+                    mask = cv2.inRange(hsv, lower_{colorspace}, upper_{colorspace})
+
+                    # Bitwise-AND mask and original image
+                    res = cv2.bitwise_and(frame,frame, mask= mask)
+
+                    cv2.imshow('frame',frame)
+                    cv2.imshow('mask',mask)
+                    cv2.imshow('res',res)
+                    k = cv2.waitKey(5) & 0xFF
+                    if k == 27:
+                        break
+
+                cv2.destroyAllWindows()
+                """)
+        
+        st.write(f"Below image shows tracking of the {colorspace} object:")
+        frame, mask, res = object_tracking(self.img, colorspaces[colorspace])
+        with st.container(border=True):
+            col1, col2, col3 = st.columns(3)
+            col1.image(frame, 'Original Frame', channels='BGR')
+            col2.image(mask, 'Mask')
+            col3.image(res, f'Tracked {colorspace} color', channels='BGR')
+
+        st.info("""
+                ⚠️ **Note**
+                > There are some noises in the image. 
+                We will see how to remove them in later chapters.
+                """)
+        
+        st.info("""
+                ⚠️ **Note**
+                
+                > This is the simplest method in object tracking.
+                Once you learn functions of contours, you can do
+                plenty of things like find centroid of this object
+                and use it to track the object, draw diagrams just
+                by moving your hand in front of camera and many other
+                funny stuffs.
+                """)
+        
+        st.write("""
+                 ## How to find HSV values to track?
+                This is a common question found in stackoverflow.com.
+                It is very simple and you can use the same function,
+                `cv2.cvtColor()`. 
+                Instead of passing an image, you just pass the BGR values
+                you want. For example, to find the HSV value of Green,
+                try following commands in Python terminal:
+                 """)
+        with st.container(border=True):
+            color = st.color_picker("Pick a color",value="#ff0000")
+            color = list(ImageColor.getcolor(f'{color}','RGB')[::-1])
+            st.code(f"""
+                    >>> color = np.uint8([[{color}]])
+                    >>> hsv = cv2.cvtColor(color,cv2.COLOR_BGR2HSV)
+                    >>> print hsv
+                    {find_hsv_values(color)}
+                    """)
+        
+        st.write("""
+                 Now you take `[H-10, 100,100]` and `[H+10, 255, 255]` 
+                 as lower bound and upper bound respectively. 
+                 Apart from this method, you can use any image 
+                 editing tools like GIMP or any online converters 
+                 to find these values, but don’t forget to adjust 
+                 the HSV ranges.
+                 
+                 ## Additional Resources
+                 ### Exercises
+                Try to find a way to extract more than one colored 
+                objects, for eg, extract red, blue, green objects 
+                simultaneously.
+                 """)
+        
+        image = blank_image(*get_shape(self.img))
+        for i in colorspaces:
+            image+=object_tracking(self.img, colorspaces[i])[-1]
+        with st.container(border=True):
+            _, col, _ = st.columns(3)
+            col.image(image, 'Expected Ouput', channels='BGR', width=200)
+            
+        with st.expander("Reveal Solution", expanded=False):
+            st.code(f"""
+            import numpy as np
+            import cv2 as cv   
+            def blank_image(height, width, channel):
+                img = np.zeros((height, width, channel), np.uint8)
+                return img
+                
+            def object_tracking(frame, colorspaces):
+                # Convert BGR to HSV
+                hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+
+                # define range of blue color in HSV
+                lower, upper = np.array(colorspaces[0]), np.array(colorspaces[1])
+
+                # Threshold the HSV image to get only blue colors
+                mask = cv.inRange(hsv, lower, upper)
+
+                # Bitwise-AND mask and original image
+                res = cv.bitwise_and(frame,frame, mask= mask)
+
+                return frame, mask, res
+                
+            img = cv.imread("{self.img_file_name}")
+            shape = img.shape
+            blank_image = blank_image(*shape)
+            colorspaces = {{
+        
+                "blue" : [[110,50,50], 
+                        [130,255,255]],
+                "green" : [[40, 40, 40],
+                        [80, 255, 255]],
+                "red" : [[0, 100, 100],
+                        [10, 255, 255]]
+                
+            }}
+            for i in colorspaces:
+                blank_image+=object_tracking(img, colorspaces[i])[-1]
+            cv.imshow('Expected Output', blank_image)
+            cv.waitKey(0)
+                """)
