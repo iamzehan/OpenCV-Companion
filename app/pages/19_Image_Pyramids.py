53e8b3be0e2ac2081e4ea3f0de1c6745897744d1
Zehan <zehan.kareem@gmail.com>
2024-03-01 18:51:56 +0600

    Added new pages from Image Processing

---
+++ app/pages/19_Image_Pyramids.py
@@ -0,0 +1,11 @@
+import streamlit as st 
+from utils.gui import footer, menu
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    st.set_page_config("Image Pyramids", page_icon='app/assets/Images/OpenCV_Logo_with_text.png')
+    menu.menu()
+    main()
+    footer.footer()
