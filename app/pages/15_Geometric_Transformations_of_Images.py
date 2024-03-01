4baf0aa91fc2f0fd2dae4ee38a710d1decda753b
Zehan <zehan.kareem@gmail.com>
2024-03-01 20:09:09 +0600

    Added page options

--- app/pages/15_Geometric_Transformations_of_Images.py
+++ app/pages/15_Geometric_Transformations_of_Images.py
@@ -2,10 +2,48 @@ import streamlit as st
 from utils.gui import footer, menu
 
 def main():
-    pass
+    st.title("""
+             Geometric Transformations on Images
+             """)
+    with st.sidebar.container(border=True):
+        st.subheader("Topics")
+        options = st.radio("Topics", options=['Introduction',
+                                    'Scaling',
+                                    'Translation',
+                                    'Rotation',
+                                    'Affine Transformation',
+                                    'Perspective Transformation'],
+                           label_visibility='collapsed')
+    if options == 'Introduction':
+        st.markdown("""
+            ## Goals
+            Learn to apply different geometric transformations to images, such as translation, rotation, affine transformation, etc.
+            You will encounter these functions: `cv2.getPerspectiveTransform`
+            ### Transformations
+            OpenCV provides two transformation functions, 
+            `cv2.warpAffine` and `cv2.warpPerspective`, with which you can perform all kinds of transformations. 
+            `cv2.warpAffine` takes a 2x3 transformation matrix, while `cv2.warpPerspective` takes a 
+            3x3 transformation matrix as input.
+            """)
+
+    elif options == 'Scaling':
+        st.markdown("## Scaling")
+
+    elif options == 'Translation':
+        st.markdown("## Translation")
+
+    elif options == 'Rotation':
+        st.markdown("## Rotation")
+
+    elif options == 'Affine Transformation':
+        st.markdown("## Affine Transformation")
+
+    elif options == 'Perspective Transformation':
+        st.markdown("## Perspective Transformation")
 
 if __name__ == '__main__':
     st.set_page_config("Geometric Transformations of Images", page_icon='app/assets/Images/OpenCV_Logo_with_text.png')
     menu.menu()
+
     main()
     footer.footer()
