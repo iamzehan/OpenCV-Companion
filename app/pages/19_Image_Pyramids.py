24701e134a0893a9fc1914a1d059e36b109d9c36
Zehan <zehan.kareem@gmail.com>
2024-03-14 00:59:49 +0600

    Adding Navigations

--- app/pages/19_Image_Pyramids.py
+++ app/pages/19_Image_Pyramids.py
@@ -1,8 +1,28 @@
 import streamlit as st 
-from utils.gui import footer, menu
+from utils.gui import footer, menu, images
 
 def main():
-    pass
+    img_pyr = images.ImagePyramids()
+    st.title("Image Pyramids")
+    
+    with st.container(border=True):
+        st.subheader("Topics")
+        options = st.radio("Options: ", 
+                        options=["Introduction", "Theory", "Image Blending using Pyramids"],
+                        horizontal=True,
+                        label_visibility="collapsed")
+    
+    with st.container(border=True):
+        
+        if options == "Introduction":
+            img_pyr.Introduction()
+            
+        elif options=="Theory":
+            img_pyr.side_bar()
+            img_pyr.Theory()
+        
+        elif options == "Image Blending using Pyramids":
+            img_pyr.ImageBlending()
 
 if __name__ == '__main__':
     st.set_page_config("Image Pyramids", page_icon='app/assets/Images/OpenCV_Logo_with_text.png')
