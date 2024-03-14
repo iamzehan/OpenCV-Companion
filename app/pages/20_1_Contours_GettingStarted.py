fb9b4d60728773ac68a58091de7e4fc6c92ed15a
Zehan <zehan.kareem@gmail.com>
2024-03-15 02:09:08 +0600

    Added option Contour Approximation Method

--- app/pages/20_1_Contours_GettingStarted.py
+++ app/pages/20_1_Contours_GettingStarted.py
@@ -2,9 +2,31 @@ import streamlit as st
 from utils.gui import footer, menu, images
 def main():
     st.title("Contours: Getting Started")
-
-    st.markdown("## contour_1: Learn to find and draw Contours")
-    st.write("Content for contour_1 goes here.")
+    getting_started= images.Contours.GettingStarted()
+    
+    with st.container(border=True):
+        
+        st.subheader("Topic")
+        options = st.radio("Options: ",
+                           options= ["Introduction",
+                                     "What are contours?",
+                                     "How to draw the contours?",
+                                     "Contour Approximation Method"],
+                           horizontal=True,
+                           label_visibility="collapsed")
+        
+    with st.container(border=True):
+        if options == "Introduction":
+            getting_started.Introduction()
+        
+        elif options == "What are contours?":
+            getting_started.What_are_Contours()
+        
+        elif options == "How to draw the contours?":
+            getting_started.How_to_Draw()
+            
+        elif options == "Contour Approximation Method":
+            getting_started.Contour_Approx_Method()
 
 if __name__ == "__main__":
     st.set_page_config("Contours in OpenCV", page_icon='app/assets/Images/OpenCV_Logo_with_text.png')
