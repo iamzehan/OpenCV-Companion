1badd9603a51421aa0c63343331bd76cb7c26d54
Zehan <zehan.kareem@gmail.com>
2024-05-13 02:53:59 +0600

    Add page structure

--- app/pages/21_1_Histograms_1.py
+++ app/pages/21_1_Histograms_1.py
@@ -2,10 +2,24 @@ import streamlit as st
 from utils.gui import footer, menu, images
 def main():
     st.title("Histograms - 1 : Find, Plot, Analyze !!!")
-
     st.markdown("Learn to find and draw Contours")
     
-
+    hist1 = images.Histograms.Histograms1()
+    functions = {
+        "Introduction" : hist1.Introduction,
+        "Find Histogram" : hist1.FindHistogram,
+        "Plotting Histogram": hist1.PlottingHistograms,
+        "Application of Mask": hist1.ApplicationOfMask 
+    }
+    with st.container(border=True):
+                st.subheader("Topics")
+                options = st.radio(label="Options: ",
+                                options=list(functions.keys()),
+                                horizontal = True,
+                                label_visibility="collapsed")
+            
+    if options: functions[options]()
+        
 if __name__ == "__main__":
     st.set_page_config("Histograms in OpenCV", page_icon='app/assets/Images/OpenCV_Logo_with_text.png')
     menu.menu()
