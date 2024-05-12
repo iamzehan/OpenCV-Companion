2da40ca8b45865d3ca8b7849f089ce63324e1ff1
Zehan <zehan.kareem@gmail.com>
2024-05-13 02:09:48 +0600

    Typo fixed

--- app/utils/gui/images.py
+++ app/utils/gui/images.py
@@ -3383,29 +3383,29 @@ class Contours:
             
 class Histograms(ImageProcessing):
     def __init__(self):
-        super().__init__
+        super().__init__()
 
 class ImageTransformations(ImageProcessing):
     def __init__(self):
-        super().__init__
+        super().__init__()
 
 class TemplateMatching(ImageProcessing):
     def __init__(self):
-        super().__init__
+        super().__init__()
 
 class HoughLineTransform(ImageProcessing):
     def __init__(self):
-        super().__init__
+        super().__init__()
     
 
 class HoughCircleTransform(ImageProcessing):
     def __init__(self):
-        super().__init__
+        super().__init__()
 
 class ImageSegmentation(ImageProcessing):
     def __init__(self):
-        super().__init__
+        super().__init__()
 
 class InteractiveForegroundExtraction(ImageProcessing):
     def __init__(self):
-        super().__init__
+        super().__init__()
