4a18b6924c7a08fc425df691400e57045ba5aff5
Zehan <zehan.kareem@gmail.com>
2024-05-14 00:45:44 +0600

    Add Histograms 3 page options

--- app/pages/21_3_Histograms_3.py
+++ app/pages/21_3_Histograms_3.py
@@ -4,7 +4,24 @@ def main():
     st.title("Histograms - 3 : 2D Histograms")
 
     st.markdown("Learn to find and plot 2D Histograms")
-
+    hist3 = images.Histograms.Histograms3()
+    functions = {
+        "Introduction" : hist3.Introduction,
+        "2D Histograms in OpenCV" : hist3.Histograms_2D_OpenCV,
+        "2D Histograms in Numpy" : hist3.Histograms_2D_Numpy,
+        "Plotting 2D Histograms" : hist3.Plotting_2D_Histograms
+     }
+    
+    with st.container(border=True):
+                st.subheader("Topics")
+                options = st.radio(label="Options: ",
+                                options=list(functions.keys()),
+                                horizontal = True,
+                                label_visibility="collapsed")
+            
+    if options:
+        functions[options]()
+        
 if __name__ == "__main__":
     st.set_page_config("Histograms in OpenCV", page_icon='app/assets/Images/OpenCV_Logo_with_text.png')
     menu.menu()
