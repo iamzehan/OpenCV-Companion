360cff4c0e0c2e453fe28e95d4410bde583edfc0
Zehan <zehan.kareem@gmail.com>
2024-05-14 00:43:06 +0600

    Add Histograms 2 page options

--- app/pages/21_2_Histograms_2.py
+++ app/pages/21_2_Histograms_2.py
@@ -2,8 +2,25 @@ import streamlit as st
 from utils.gui import footer, menu, images
 def main():
     st.title("Histograms - 2: Histogram Equalization")
-
-    st.markdown("Learn to Equalize Histograms to get better contrast for images")
+    hist2 = images.Histograms.Histograms2()
+    
+    functions = {
+        "Introduction" : hist2.Introduction,
+        "Theory" : hist2.Theory,
+        "Histograms Equalization" : hist2.HistogramsEqualization,
+        "CLAHE": hist2.CLAHE
+     }
+    
+    with st.container(border=True):
+                st.subheader("Topics")
+                options = st.radio(label="Options: ",
+                                options=list(functions.keys()),
+                                horizontal = True,
+                                label_visibility="collapsed")
+            
+    if options:
+        functions[options]()
+    
 
 if __name__ == "__main__":
     st.set_page_config("Histograms in OpenCV", page_icon='app/assets/Images/OpenCV_Logo_with_text.png')
