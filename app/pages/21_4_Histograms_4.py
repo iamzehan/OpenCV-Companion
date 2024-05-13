b8bcd11dd9f306881f0b71392f571f873440e5e0
Zehan <zehan.kareem@gmail.com>
2024-05-14 00:47:18 +0600

    Add Histograms 4 page options

--- app/pages/21_4_Histograms_4.py
+++ app/pages/21_4_Histograms_4.py
@@ -2,7 +2,22 @@ import streamlit as st
 from utils.gui import footer, menu, images
 def main():
     st.title("Histogram - 4 : Histogram Backprojection")
-    st.markdown("	Learn histogram backprojection to segment colored objects")
+    hist4 = images.Histograms.Histograms4()
+    functions = {
+        "Introduction" : hist4.Introduction,
+        "Algorithm in Numpy" : hist4.Algorithm_in_Numpy,
+        "Backprojection in OpenCV": hist4.Backprojection_in_OpenCV
+     }
+    
+    with st.container(border=True):
+                st.subheader("Topics")
+                options = st.radio(label="Options: ",
+                                options=list(functions.keys()),
+                                horizontal = True,
+                                label_visibility="collapsed")
+            
+    if options:
+        functions[options]()
 
 if __name__ == "__main__":
     st.set_page_config("Histograms in OpenCV", page_icon='app/assets/Images/OpenCV_Logo_with_text.png')
